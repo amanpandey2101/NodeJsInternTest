6d498fc143b7b0ac37587e07e1325e0d53286b36
Aman <paman7681@gmail.com>
2023-05-07 01:23:02 +0530

    final commit

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -28,10 +28,9 @@ function Navbar() {
         <div class="hidden sm:ml-6 sm:block">
           <div class="flex space-x-4">
           
-            <a href="#" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Dashboard</a>
-            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Team</a>
-            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Projects</a>
-            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Calendar</a>
+
+          
+          
           </div>
         </div>
       </div>
