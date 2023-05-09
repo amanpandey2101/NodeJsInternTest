302ebad505d20b83849becc467978eb3db1897e2
Aman <paman7681@gmail.com>
2023-05-09 19:20:57 +0530

    loader

--- frontend/src/components/Table.js
+++ frontend/src/components/Table.js
@@ -1,23 +1,36 @@
 import React, { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 import "./style.css"
+import { PulseLoader } from "react-spinners";
+
 function Table() {
   const [cryptoPrices, setCryptoPrices] = useState([]);
+  const [loading, setLoading] = useState(true);
 
+  
   useEffect(() => {
+    
     fetch("https://hodlinfo-d2ks.onrender.com/api/crypto")
-      .then((response) => response.json())
-      .then((data) => setCryptoPrices(data))
+      .then((response) => response.json(),setLoading(true))
+      .then((data) => setCryptoPrices(data),setLoading(false) )
       .catch((error) => console.error("Error fetching data:", error));
-  }, []);
-
+     
+      
+    }, []);
+    
+   
   return (
     <div className="flex flex-col w-full  p-16 max-[768px]:p-4 ">
+      <div className="text-center">
+
+      {loading && <PulseLoader color={"white"} size={10} className='mb-2' />}
+      </div>
       <div className="-my-2 sm:-mx-6 lg:-mx-8 overflow-x-auto">
         <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
           <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
             <table className="min-w-full divide-y divide-gray-200 ">
               <table className="min-w-full divide-y divide-gray-200 ">
+              
                 <thead className="bg-[#191D28]">
                   <tr>
                     <th className="px-6 py-3 text-left text-xl font-medium text-gray-400 uppercase tracking-wider">
